6d526f6a6c79ffc968cd78c1bed2fe1671505f53
Ali Haider <alihaider11040@gmail.com>
2022-07-26 16:22:39 +0500

    static files and dynamic markup

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include("myapp.urls")),
+    path('newyearapp/', include("newyearapp.urls")),
 ]
