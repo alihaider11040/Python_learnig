b673d2b6708ff13a52487f9af3b68cf64dcdee75
Ali Haider <alihaider11040@gmail.com>
2022-07-27 10:58:40 +0500

    Session Management

--- myproject/tasks/views.py
+++ myproject/tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 tasks=['a','b','c']
@@ -9,11 +11,26 @@ class NewTaskForm(forms.Form):
     priority = forms.IntegerField(label='Priority', min_value=1,max_value=10)
 
 def index(request):
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
     return render(request,'tasks/index.html',{
-        "tasks":tasks
+        "tasks":request.session['tasks'] 
     })
     
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm (request.POST)
+        if form.is_valid():
+
+            task = form.cleaned_data["task"]
+            request.session["tasks"]+= [task]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            render(request,'tasks/add.html',{
+                "form":form
+            })
+    
     return render(request, 'tasks/add.html',{
 
         'form': NewTaskForm()
